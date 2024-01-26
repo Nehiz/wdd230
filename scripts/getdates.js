b106e1171d4e594cfb1868be5e8770d6ac548928
Circuit19 <efehi.nehikhare@gmail.com>
2024-01-26 10:05:31 +0100

    index file

---
+++ scripts/getdates.js
@@ -0,0 +1,44 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
+
+
+const options = {year: 'numeric'}
+
+document.querySelector('#year').textContent = new Date().toLocaleDateString('pt-BR', options);
+
+ 
+
+const oLastModif = new Date(document.lastModified);
+
+document.querySelector('#lastModified').textContent = oLastModif;
+
+ 
+
+const currentPageURL = window.location.href;
+
+ 
+
+const navLinks = document.querySelectorAll('nav a');
+
+ 
+
+for(var i=0; i< navLinks.length; i++) 
+
+{
+
+    if(navLinks[i].href === currentPageURL)
+
+    {
+
+        navLinks[i].classList.add('active');
+
+    }
+
+}
+
+

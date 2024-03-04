79eeea07448abc20b6407dd2a3fb29412815736e
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-04 13:32:26 +0100

    Chamber of Commerce Home Page Continued

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -12,6 +12,22 @@ hamButton.addEventListener('click', () => {
 
 
 
+const modeButton = document.querySelector("#mode");
+const Body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		Body.style.background = "#000";
+		Body.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		Body.style.background = "#fff";
+		Body.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
+
 
 const options = {year: 'numeric'}
 

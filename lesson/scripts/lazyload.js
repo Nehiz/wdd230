d33c0a0e99db506d9c1510c1e53de72b2834e068
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-11 14:48:25 +0100

    Lazy Loading

---
+++ lesson/scripts/lazyload.js
@@ -0,0 +1,33 @@
+const options = {year: 'numeric'}
+
+document.querySelector('#year').textContent = new Date().toLocaleDateString('pt-BR', options);
+
+ 
+
+const oLastModif = new Date(document.lastModified);
+
+document.querySelector('#lastModified').textContent = oLastModif;
+
+ 
+
+const currentPageURL = window.location.href;
+
+ 
+
+const navLinks = document.querySelectorAll('nav a');
+
+ 
+
+for(var i=0; i< navLinks.length; i++) 
+
+{
+
+    if(navLinks[i].href === currentPageURL)
+
+    {
+
+        navLinks[i].classList.add('active');
+
+    }
+
+}
